772abfc5f9bf658efff3a30d6f8efc7ba89f099c
kribsin <57453702+kribsin@users.noreply.github.com>
2021-10-07 13:53:20 +0530

    main
    main

---
+++ lib/utility/app_url.dart
@@ -0,0 +1,7 @@
+class AppUrl {
+  static const String baseUrl = 'https://kpi.kribstech.com/';
+
+  static const String login = baseUrl + '/login';
+  static const String register = baseUrl + '/register';
+  static const String forgotPassword = baseUrl + '/forgot_password';
+}
